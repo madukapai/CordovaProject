ea7e12ef832d67516e09561ea553c9bdeabc267e
maduka <maduka@MADUKA-NB>
2016-10-15 17:01:54 +0800

    多語系功能的加入

---
+++ CordovaProject/www/scripts/language.js
@@ -0,0 +1,16 @@
+﻿function languageInit(strLanguage) {
+    var langs = ['zh-TW', 'en-US'];
+    var langJS = null;
+
+    if (langs.indexOf(strLanguage) > -1)
+        $.getJSON('language/' + strLanguage + '.json', translate);
+    else
+        $.getJSON('language/zh-TW.json', translate);
+
+    function translate(jsdata) {
+        $("[lkey]").each(function (index) {
+            var strTr = jsdata[$(this).attr('lkey')];
+            $(this).html(strTr);
+        });
+    }
+}
